352941286a5b2469891b6f00c091203229b1d9c5
Sharky <42697462+SharkyTheKing@users.noreply.github.com>
2019-05-05 19:57:46 -0600

    Sayings - test

---
+++ sayings/__init__.py
@@ -0,0 +1,4 @@
+from .sayings import Sayings
+
+def setup(bot):
+    bot.add_cog(Sayings())
---
+++ sayings/sayings.py
@@ -0,0 +1,26 @@
+from redbot.core import commands, checks
+
+class Sayings(commands.Cog):
+#People Group
+    @commands.group()
+    async def people(self, ctx):
+        """People"""
+    
+    @people.command()
+    async def soul(self, ctx):
+        """A comment about Soul"""
+        await ctx.send("Soul is a troll, for he is the king of them all. He's also an asshole.")
+    
+    @people.command()
+    async def kowlin(self, ctx):
+        """A comment about the Wyvern"""
+        await ctx.send("Kowlin, for he is a lovely Wyvern who breaths fire, has your back and knows how to make shit look easy when it's really not.")
+
+#phrase group
+    @commands.group()
+    async def phrase(self, ctx):
+        """Phrases"""
+    @phrase.command()
+    async def potato(self, ctx):
+        """Potatos"""
+        await ctx.send("You are a potato.")
