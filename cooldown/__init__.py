6959e6e42674755a5a7e854971ae6f0d2aebe474
Sharky <42697462+SharkyTheKing@users.noreply.github.com>
2019-05-07 20:06:30 -0600

    Cooldown Testing

---
+++ cooldown/__init__.py
@@ -0,0 +1,4 @@
+from .time import Time
+
+def setup(bot):
+    bot.add_cog(Testing())
---
+++ cooldown/cooldown.py
@@ -0,0 +1,15 @@
+import discord
+import time
+import asyncio
+from redbot.core import commands, check
+from redbot.core.data_manager import bundled_data_path
+
+class Cooldown(commands.cog):
+    """Testing things out"""
+
+
+#Command placement
+    @commands.group()
+    async def cooldown(self,ctx):
+        """Cooldown placement"""
+        await ctx.send("Nothing here. Curious what this is though? Working on learning cooldowns for commands.")
