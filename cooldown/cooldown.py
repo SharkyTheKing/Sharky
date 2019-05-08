078796dc2a5b34378b36b09e46e4921e30c85e31
Sharky <42697462+SharkyTheKing@users.noreply.github.com>
2019-05-07 22:18:04 -0600

    Update
    Adjusted commands, added a new one, testing out user and guild

--- cooldown/cooldown.py
+++ cooldown/cooldown.py
@@ -4,15 +4,19 @@ import asyncio
 from redbot.core import commands, checks
 from redbot.core.data_manager import bundled_data_path
 
-BaseCog = getattr(commands, "Cog", object)
-
-class Cooldown(Basecog):
+class Cooldown(commands.Cog):
     """Testing things out"""
 
 
 #Import datetime as seen on line 2, follow the command line below to a command down for a user
-    @commands.cooldown(1, 20, discord.ext.commands.BucketType.user)
+    @commands.cooldown(1, 15, discord.ext.commands.BucketType.user)
     @commands.command()
-    async def testing(self,ctx):
-        """Cooldown placement"""
+    async def testing(self, ctx):
+        """Cooldown for user"""
         await ctx.send("Nothing here. Curious what this is though? Learning how to make cooldowns per commands and how to set cooldowns. :D")
+
+    @commands.cooldown(1, 15, discord.ext.commands.BucketType.guild)
+    @commands.command()
+    async def guildtest(self, ctx):
+        """Cool down for guild"""
+        await ctx.send("Let's see if this works")
