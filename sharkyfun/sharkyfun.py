cfa017e8e89d922a73256b5153aeb292373df013
Sharky <42697462+SharkyTheKing@users.noreply.github.com>
2019-05-14 02:07:48 -0600

    Update sharkyfun.py

--- sharkyfun/sharkyfun.py
+++ sharkyfun/sharkyfun.py
@@ -5,11 +5,10 @@ from redbot.core.data_manager import bundled_data_path
 
 class SharkyFun(commands.Cog):
     """A personal fun cog full of Shark/Fishy related humor"""
-
-@commands.cooldown(1, 15, discord.ext.commands.BucketType.guild)
-@commands.command()
-async def banhammer(self, ctx):
-    """Swing that hammer"""
-    file = discord.File(str(bundled_data_path(self) / "catban.png"))
-    async with ctx.typing():
-        await ctx.send(files=[file])
+    @commands.cooldown(1, 15, discord.ext.commands.BucketType.user)
+    @commands.command()
+    async def banhammer(self, ctx):
+        """Swing that hammer"""
+        file = discord.File(str(bundled_data_path(self) / "catban.png"))
+        async with ctx.typing():
+            await ctx.send(files=[file])
