d6a9ab74aff497b5634efc3f47a76397a69765ff
Sharky <42697462+SharkyTheKing@users.noreply.github.com>
2019-05-17 23:31:36 -0600

    Bug Fixes + Updating
    Massive update with command SharkInfo,  working days counter.
    
    Removed all mod/owner checks. Possibly will readd depending on how I feel the cog is going to be used personally.
    
    New avatar commands! One for user avatars for people who aren't on the server the bot is on. Fixed up the avatar command that goes for people ON the server to include account information as well.
    
    uage command! A quick simple way to find someone's account age/join age on a server.

--- sharkytools/sharkytools.py
+++ sharkytools/sharkytools.py
@@ -5,7 +5,7 @@ import asyncio
 
 class SharkyTools(commands.Cog):
     """Sharky Tools"""
-
+#  Sharky's Userinfo twist
     @commands.command(name="sharkinfo", aliases=['pinfo'])
     @commands.guild_only()
     @commands.bot_has_permissions(embed_links=True, send_messages=True)
@@ -23,10 +23,20 @@ class SharkyTools(commands.Cog):
 
         member_role = sorted(member.roles)[1:]  # this and line 35 are required for role formats
         if member_role:  # this lets us format the roles properly so theyr'e named correctly
-            member_role = ", ".join([x.name for x in member_role]) 
-        joined = member.joined_at.strftime("%d %b %Y %H:%M")
-        created = member.created_at.strftime("%d %b %Y %H:%M")     
-        notice = "Puppy Shark"
+            member_role = ", ".join([x.name for x in member_role])     
+        notice = "Thank you for using Sharky's Tools"
+        
+        #  Tie this together with created_on and joined_on
+        #  Credit to Red Core Userinfo command: I am not this smart yet :eyes:
+        joined_at = member.joined_at  # This is REQUIRED for 'since_joined`
+        member_joined = member.joined_at.strftime("%d %b %Y %H:%M")  # When the user joined the server
+        since_joined = (ctx.message.created_at - joined_at).days  # Since the user joined the server in days
+        member_created = member.created_at.strftime("%d %b %Y %H:%M")  # When the user account was created
+        since_created = (ctx.message.created_at - member.created_at).days  # Since the user account was created in days
+
+        created_on = ("{}\n({} days ago)").format(member_created, since_created)  # Formats when the account was created into a proper day message
+        joined_on = ("{}\n({} days ago)").format(member_joined, since_joined)  # Formats when the account joined the server into a proper day message
+
     #   Embeds
         embed = discord.Embed(
             color=0xEE2222,
@@ -45,12 +55,12 @@ class SharkyTools(commands.Cog):
                 value=(f"{member_mention} is a bot"),
                 inline=False)
         embed.add_field(
-            name="Joined Date:",
-            value=f'{joined}',
+            name="Account Creation:",
+            value=f'{created_on}',
             inline=True)
         embed.add_field(
-            name="Account Creation:",
-            value=f'{created}',
+            name="Joined Date:",
+            value=f'{joined_on}',
             inline=True)
         embed.add_field(
             name='Roles:',
@@ -62,7 +72,6 @@ class SharkyTools(commands.Cog):
                 value="<#{0.id}> (ID: {0.id})".format(member_voice.channel),
                 inline=False)
     # Non-fielded embedsets
-    #    embed.set_image(url=member_avatar) - Ignore this entirely, just something extra
         embed.set_footer(
             text=f'{notice}')
         embed.set_thumbnail(
@@ -126,10 +135,10 @@ class SharkyTools(commands.Cog):
                 name=f'They are not banned from the server.', 
                 value=f'{member} ({mid})')
             return await ctx.send(embed=embed)
+
 #   User Avatar
     @commands.command()
     @commands.bot_has_permissions(embed_links=True, send_messages=True)
-    @checks.mod_or_permissions(manage_messages=True)
     @commands.guild_only()
     async def av(self, ctx, *, user: discord.Member):
         """A user's avatar"""
@@ -138,8 +147,17 @@ class SharkyTools(commands.Cog):
         user_name = user.name  # Default Discord name
         user_id = user.id  # USERID
         user_av = user.avatar_url_as(static_format="png")  # Avatar, static is formated as png
-        joined = user.joined_at.strftime("%d %b %Y %H:%M")
-        created = user.created_at.strftime("%d %b %Y %H:%M")
+
+        #  Tie this together with created_on and joined_on
+        #  Credit to Red Core Userinfo command: I am not this smart yet :eyes:
+        joined_at = user.joined_at  # This is REQUIRED for 'since_joined`
+        user_joined = user.joined_at.strftime("%d %b %Y %H:%M")  # When the user joined the server
+        since_joined = (ctx.message.created_at - joined_at).days  # Since the user joined the server in days
+        user_created = user.created_at.strftime("%d %b %Y %H:%M")  # When the user account was created
+        since_created = (ctx.message.created_at - user.created_at).days  # Since the user account was created in days
+
+        created_on = ("{}\n({} days ago)").format(user_created, since_created)  # Formats when the account was created into a proper day message
+        joined_on = ("{}\n({} days ago)").format(user_joined, since_joined)  # Formats when the account joined the server into a proper day message
 
         embed = discord.Embed(
             color=0xEE2222,
@@ -154,15 +172,37 @@ class SharkyTools(commands.Cog):
             value=f'{user_name}#{user_disc}'
         )
         embed.add_field(
-            name=f'Join Date:',
-            value=f'{joined}'
+            name=f'Account Age:',
+            value=f'{created_on}'
         )
         embed.add_field(
-            name=f'Account Age:',
-            value=f'{created}'
+            name=f'Join Date:',
+            value=f'{joined_on}'
         )
         embed.set_image(url=user_av)
         await ctx.send(embed=embed)
+
+# Grabbing ANY user's avatar. This is hidden on purpose
+    @commands.command()
+    @commands.bot_has_permissions(embed_links=True, send_messages=True)
+    async def uav(self, ctx, *, user):
+        """Get a user's avatar even if they aren't on the server"""
+
+        try:
+            #  argument setups
+            user_acc = await ctx.bot.fetch_user(user)
+            user_av = user_acc.avatar_url_as(static_format="png")
+            user_name = user_acc.name
+            user_disc = user_acc.discriminator
+            #  embed
+            embed = discord.Embed(
+                color=0xEE2222,
+                title=f'Avatar Info: {user_name}#{user_disc}'
+            )
+            embed.set_image(url=user_av)
+            await ctx.send(embed=embed)
+        except discord.HTTPException:
+            await ctx.send("Please use a valid UserID")
 
 #   Fetching UserIDs for mobile users.
     @commands.command()
@@ -171,7 +211,7 @@ class SharkyTools(commands.Cog):
         """Get a user's ID"""
         u_id = user.id
         u_name = user.name
-        await ctx.send(f'{u_name}\'s ID:\n{u_id}')
+        await ctx.send(f'{u_name}\'s ID: {u_id}')
 #   Guild ID
     @commands.command()
     @commands.guild_only()
@@ -180,24 +220,51 @@ class SharkyTools(commands.Cog):
         guildid = ctx.guild.id
         await ctx.send(f'Guild\'s ID: {guildid}')
 #   Format Bot Invites
-    @commands.command()
     @commands.guild_only()
+    @commands.command()
     async def binv(self, ctx, *, inv: discord.User):
-        """
-        Use a BOT's ID and it'll format it out into a link for you.
-        It will DM it to you, incase the bot owner does not want their invite in public chatroom *yes that's ironic*.
-        """
+        """Get a bot's invite link by copying the ID of bot"""
         bot_is = inv.bot
         bot_id = inv.id
-        am = ctx.author.mention
         author = ctx.author
-    
-        if bot_is is True:
-            return await author.send(f'https://discordapp.com/oauth2/authorize?client_id={bot_id}&scope=bot')
-        await author.send(f"Try again {am}")
+        am = ctx.author.mention
         try:
             await ctx.message.delete()
+            if bot_is is True:
+                return await author.send(f'https://discordapp.com/oauth2/authorize?client_id={bot_id}&scope=bot')
+            await author.send(f"Try again {am}")
         except discord.errors.Forbidden:
             await ctx.send("``Error in command 'binv'. Check your console or logs for details.``")
             await asyncio.sleep(3)
             await ctx.send("Sike. Just fix your permissions")
+#  Find a user's account age and join age.
+    @commands.guild_only()
+    @commands.command()
+    @commands.bot_has_permissions(embed_links=True, send_messages=True)
+    async def uage(self, ctx, *, user: discord.Member):
+        """Find out the person's account age and join date!"""
+        user_name = user.name
+        user_disc = user.discriminator
+        user_av = user.avatar_url_as(static_format="png")
+        joined_at = user.joined_at  # This is REQUIRED for 'since_joined`
+        user_joined = user.joined_at.strftime("%d %b %Y %H:%M")  # When the user joined the server
+        since_joined = (ctx.message.created_at - joined_at).days  # Since the user joined the server in days
+        user_created = user.created_at.strftime("%d %b %Y %H:%M")  # When the user account was created
+        since_created = (ctx.message.created_at - user.created_at).days  # Since the user account was created in days
+        created_on = ("{}\n({} days ago)").format(user_created, since_created)  # Formats when the account was created into a proper day message
+        joined_on = ("{}\n({} days ago)").format(user_joined, since_joined)  # Formats when the account joined the server into a proper day message
+
+        embed = discord.Embed(
+            color=0xEE2222,
+            title=f"{user_name}#{user_disc}'s Account Date:"
+        )
+        embed.add_field(
+            name=f'Account Age:',
+            value=f'{created_on}'
+        )
+        embed.add_field(
+            name=f'Join Date:',
+            value=f'{joined_on}'
+        )
+        embed.set_thumbnail(url=user_av)
+        await ctx.send(embed=embed)
