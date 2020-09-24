5ead1bb751ecf0963ec5232344b7d65ace5a8966
Predeactor <predeactor0@gmail.com>
2020-09-24 19:48:00 +0200

    Black be like painful to fix.

--- lockdown/core.py
+++ lockdown/core.py
@@ -267,9 +267,7 @@ class Lockdown(BASECOG):
         channel_embed = list(pagify(chan, page_length=1000))
         for idx, page in enumerate(channel_embed, start=1):
             embed = discord.Embed(
-                color=await ctx.embed_color(),
-                title="Lockdown Settings:",
-                description=chan,
+                color=await ctx.embed_color(), title="Lockdown Settings:", description=chan,
             )
             embed.add_field(
                 name="Lock Message:", value=lock_message if lock_message else "None set"
