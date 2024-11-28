ff29d173832a2445e7c8e7dcc3ac71f152268ef8
Sharky <nikroybal13@gmail.com>
2024-11-28 12:05:33 -0700

    Remove MorseShark + remove workflow

--- morseshark/__init__.py
+++
@@ -1,9 +0,0 @@
-from .core import MorseShark
-
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data or metadata about users."
-)
-
-
-async def setup(bot):
-    await bot.add_cog(MorseShark(bot))
